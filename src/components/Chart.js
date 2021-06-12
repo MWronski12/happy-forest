90b07d4a8419ea697a97b1a444302461386f5c4b
MWronski12 <wronski.mikolaj@gmail.com>
2021-06-13 00:33:09 +0200

    Happy Forest Frontend

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -23,11 +23,17 @@ const Chart = ({ sensor, title, state }) => {
   const data = measurements.filter((m) => m.deviceId === state.device.id);
 
   const chartData = {
-    labels: data.map((m) => m.timestamp),
+    labels:
+      data.length > 10
+        ? data.map((m) => m.timestamp).slice(-10)
+        : data.map((m) => m.timestamp),
     datasets: [
       {
-        label: 'dataset',
-        data: data.map((m) => m[sensor]),
+        label: "dataset",
+        data:
+          data.length > 10
+            ? data.map((m) => m[sensor]).slice(-10)
+            : data.map((m) => m[sensor]),
         fill: false,
         backgroundColor: theme.palette.secondary.main,
         borderColor: theme.palette.secondary.main,
